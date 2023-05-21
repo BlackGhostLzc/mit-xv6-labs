0110ee148ad905fef77dd990cb5cf50aa3eb6719
“BlackGhostLzc” <2427324519@qq.com>
2023-05-21 16:06:54 +0800

    ready to submit my lab

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -7,14 +7,17 @@
 #include "spinlock.h"
 #include "proc.h"
 
+#include "sysinfo.h"
+
 uint64
 sys_exit(void)
 {
   int n;
-  if(argint(0, &n) < 0)
+  // 获取第 a0 这个参数，写入变量n中
+  if (argint(0, &n) < 0)
     return -1;
   exit(n);
-  return 0;  // not reached
+  return 0; // not reached
 }
 
 uint64
@@ -33,7 +36,7 @@ uint64
 sys_wait(void)
 {
   uint64 p;
-  if(argaddr(0, &p) < 0)
+  if (argaddr(0, &p) < 0)
     return -1;
   return wait(p);
 }
@@ -44,10 +47,10 @@ sys_sbrk(void)
   int addr;
   int n;
 
-  if(argint(0, &n) < 0)
+  if (argint(0, &n) < 0)
     return -1;
   addr = myproc()->sz;
-  if(growproc(n) < 0)
+  if (growproc(n) < 0)
     return -1;
   return addr;
 }
@@ -58,12 +61,14 @@ sys_sleep(void)
   int n;
   uint ticks0;
 
-  if(argint(0, &n) < 0)
+  if (argint(0, &n) < 0)
     return -1;
   acquire(&tickslock);
   ticks0 = ticks;
-  while(ticks - ticks0 < n){
-    if(myproc()->killed){
+  while (ticks - ticks0 < n)
+  {
+    if (myproc()->killed)
+    {
       release(&tickslock);
       return -1;
     }
@@ -78,7 +83,7 @@ sys_kill(void)
 {
   int pid;
 
-  if(argint(0, &pid) < 0)
+  if (argint(0, &pid) < 0)
     return -1;
   return kill(pid);
 }
@@ -94,4 +99,45 @@ sys_uptime(void)
   xticks = ticks;
   release(&tickslock);
   return xticks;
+}
+
+uint64
+sys_trace(void)
+{
+  // 这是在内核态了,如何获取参数呢？
+  // 需要的参数在寄存器中(获取mask),也就是寄存器a0
+  int mask;
+  // int argint(int n, int *ip)
+  if (argint(0, &mask) < 0)
+  {
+    return -1;
+  }
+  myproc()->mask = mask;
+  return 0;
+}
+
+uint64
+sys_sysinfo(void)
+{
+  // 有一个参数，表示 sysinfo 结构体的地址
+  uint64 addr;
+  if (argaddr(0, &addr) < 0)
+  {
+    return -1;
+  }
+  // freemem    还剩下多少个字节(物理内存)
+  // nproc      number of processes whose state is not UNUSED
+  uint64 np;
+  uint64 fm;
+  np = cal_proc_unused();
+  fm = cal_freemem();
+  struct proc *p = myproc();
+  struct sysinfo tmp;
+  tmp.freemem = fm;
+  tmp.nproc = np;
+  if (copyout(p->pagetable, addr, (char *)(&tmp), sizeof(tmp)) < 0)
+  {
+    return -1;
+  }
+  return 0;
 }
--- user/trace.c
+++ user/trace.c
@@ -3,25 +3,32 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
   int i;
   char *nargv[MAXARG];
 
-  if(argc < 3 || (argv[1][0] < '0' || argv[1][0] > '9')){
+  if (argc < 3 || (argv[1][0] < '0' || argv[1][0] > '9'))
+  {
     fprintf(2, "Usage: %s mask command\n", argv[0]);
     exit(1);
   }
-
-  if (trace(atoi(argv[1])) < 0) {
+  // 这是一个用户态的进程，需要系统调用.          trace是user.h中申明的一个函数.
+  // trace在这里是一个封装好的系统调用，它可以来设置需要追踪的系统调用，然后再下面的exec中，程序继承了这个trace追踪的系统调用，然后每一行都要打印出来。
+  // 难道不需要设置一个全局变量来表示需要追踪哪些系统调用号吗？
+  // 这一点有些犹豫，毕竟我在proc结构体里面没有找到这个变量
+  // 没有自己加一个就可以了(其实我的猜想是正确的，可是不知道为什么却不敢加这一行)
+  if (trace(atoi(argv[1])) < 0)
+  {
     fprintf(2, "%s: trace failed\n", argv[0]);
     exit(1);
   }
-  
-  for(i = 2; i < argc && i < MAXARG; i++){
-    nargv[i-2] = argv[i];
+
+  for (i = 2; i < argc && i < MAXARG; i++)
+  {
+    nargv[i - 2] = argv[i];
   }
+  // 要执行的程序
   exec(nargv[0], nargv);
   exit(0);
 }
