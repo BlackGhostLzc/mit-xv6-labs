62d54546278e2527323e049fdd6851544003a684
“BlackGhostLzc” <2427324519@qq.com>
2023-06-03 13:53:48 +0800

    ready to submit my lab

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -11,10 +11,10 @@ uint64
 sys_exit(void)
 {
   int n;
-  if(argint(0, &n) < 0)
+  if (argint(0, &n) < 0)
     return -1;
   exit(n);
-  return 0;  // not reached
+  return 0; // not reached
 }
 
 uint64
@@ -33,7 +33,7 @@ uint64
 sys_wait(void)
 {
   uint64 p;
-  if(argaddr(0, &p) < 0)
+  if (argaddr(0, &p) < 0)
     return -1;
   return wait(p);
 }
@@ -44,10 +44,10 @@ sys_sbrk(void)
   int addr;
   int n;
 
-  if(argint(0, &n) < 0)
+  if (argint(0, &n) < 0)
     return -1;
   addr = myproc()->sz;
-  if(growproc(n) < 0)
+  if (growproc(n) < 0)
     return -1;
   return addr;
 }
@@ -58,18 +58,21 @@ sys_sleep(void)
   int n;
   uint ticks0;
 
-  if(argint(0, &n) < 0)
+  if (argint(0, &n) < 0)
     return -1;
   acquire(&tickslock);
   ticks0 = ticks;
-  while(ticks - ticks0 < n){
-    if(myproc()->killed){
+  while (ticks - ticks0 < n)
+  {
+    if (myproc()->killed)
+    {
       release(&tickslock);
       return -1;
     }
     sleep(&ticks, &tickslock);
   }
   release(&tickslock);
+  backtrace();
   return 0;
 }
 
@@ -78,7 +81,7 @@ sys_kill(void)
 {
   int pid;
 
-  if(argint(0, &pid) < 0)
+  if (argint(0, &pid) < 0)
     return -1;
   return kill(pid);
 }
@@ -94,4 +97,33 @@ sys_uptime(void)
   xticks = ticks;
   release(&tickslock);
   return xticks;
+}
+
+uint64 sys_sigalarm()
+{
+  struct proc *p = myproc();
+  int tick;
+  uint64 handler_addr;
+  if (argint(0, &tick) < 0)
+  {
+    return -1;
+  }
+  if (argaddr(1, &handler_addr) < 0)
+  {
+    return -1;
+  }
+
+  p->ticks = tick;
+  p->alarm_handler = handler_addr;
+  p->ticks_passed = 0;
+
+  return 0;
+}
+
+uint64 sys_sigreturn()
+{
+  struct proc *p = myproc();
+  *(struct trapframe *)(p->trapframe) = *(struct trapframe *)(p->alarm_frame);
+  p->alarm_on = 1;
+  return 0;
 }
