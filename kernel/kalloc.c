dee2dd6b554cc6ba1eef494964d9edf853f80be6
“BlackGhostLzc” <2427324519@qq.com>
2023-07-14 09:43:39 +0800

    Already finished this lab

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -10,51 +10,75 @@
 #include "defs.h"
 
 void freerange(void *pa_start, void *pa_end);
+void decrease_ref(uint64 pa);
+void increase_ref(uint64 pa);
 
 extern char end[]; // first address after kernel.
                    // defined by kernel.ld.
 
-struct run {
+struct run
+{
   struct run *next;
 };
 
-struct {
+int ref_cnt[PHYSTOP / PGSIZE];
+struct spinlock ref_lock;
+
+struct
+{
   struct spinlock lock;
   struct run *freelist;
 } kmem;
 
-void
-kinit()
+void kinit()
 {
   initlock(&kmem.lock, "kmem");
-  freerange(end, (void*)PHYSTOP);
+  initlock(&ref_lock, "ref_cnt");
+  freerange(end, (void *)PHYSTOP);
 }
 
-void
-freerange(void *pa_start, void *pa_end)
+void freerange(void *pa_start, void *pa_end)
 {
   char *p;
-  p = (char*)PGROUNDUP((uint64)pa_start);
-  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
+  p = (char *)PGROUNDUP((uint64)pa_start);
+  for (; p + PGSIZE <= (char *)pa_end; p += PGSIZE)
+  {
+    ref_cnt[(uint64)p / PGSIZE] = 1;
     kfree(p);
+  }
 }
 
 // Free the page of physical memory pointed at by v,
 // which normally should have been returned by a
 // call to kalloc().  (The exception is when
 // initializing the allocator; see kinit above.)
-void
-kfree(void *pa)
+void kfree(void *pa)
 {
   struct run *r;
 
-  if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
+  if (((uint64)pa % PGSIZE) != 0 || (char *)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
 
+  acquire(&ref_lock);
+  if (ref_cnt[(uint64)pa / PGSIZE] <= 0)
+  {
+    panic("Impossible for page references less than 1\n");
+  }
+  release(&ref_lock);
+
+  decrease_ref((uint64)pa);
+
+  acquire(&ref_lock);
+  if (ref_cnt[(uint64)pa / PGSIZE] >= 1)
+  {
+    release(&ref_lock);
+    return;
+  }
   // Fill with junk to catch dangling refs.
+  release(&ref_lock);
   memset(pa, 1, PGSIZE);
 
-  r = (struct run*)pa;
+  r = (struct run *)pa;
 
   acquire(&kmem.lock);
   r->next = kmem.freelist;
@@ -72,11 +96,44 @@ kalloc(void)
 
   acquire(&kmem.lock);
   r = kmem.freelist;
-  if(r)
+  if (r)
     kmem.freelist = r->next;
   release(&kmem.lock);
 
-  if(r)
-    memset((char*)r, 5, PGSIZE); // fill with junk
-  return (void*)r;
+  if (r)
+  {
+    memset((char *)r, 5, PGSIZE); // fill with junk
+    acquire(&ref_lock);
+    if (ref_cnt[(uint64)r / PGSIZE] != 0)
+    {
+      panic("Impossible when kalloc ref_cnt is not 0 \n");
+    }
+    ref_cnt[(uint64)r / PGSIZE] = 1;
+    release(&ref_lock);
+  }
+  return (void *)r;
+}
+
+void increase_ref(uint64 pa)
+{
+  if (pa >= PHYSTOP)
+  {
+    panic("increase ref_cnt panic\n");
+  }
+  acquire(&ref_lock);
+  int pn = pa / PGSIZE;
+  ref_cnt[pn] += 1;
+  release(&ref_lock);
+}
+
+void decrease_ref(uint64 pa)
+{
+  if (pa >= PHYSTOP)
+  {
+    panic("increase ref_cnt panic\n");
+  }
+  acquire(&ref_lock);
+  int pn = pa / PGSIZE;
+  ref_cnt[pn] -= 1;
+  release(&ref_lock);
 }
